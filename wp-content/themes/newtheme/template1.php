eb40a1fddd98aec425f3ac3d198074d858e96790
Cyber Valdez <wowcyber@yahoo.com>
2015-01-27 13:03:45 +0800

    added new template file: "template1.php"

---
+++ wp-content/themes/newtheme/template1.php
@@ -0,0 +1,16 @@
+<?php
+/** Template Name: template1
+ * Created by PhpStorm.
+ * User: trial
+ * Date: 1/27/2015
+ * Time: 12:59 PM
+ */
+?>
+
+<?php get_header(); ?>
+
+
+
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
