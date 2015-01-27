601aa8ee8cffc08333e8e1648c6ab1dc8d1d4f54
Cyber Valdez <wowcyber@yahoo.com>
2015-01-27 18:09:02 +0800

    Added design template to template1.php changed footer, header and style.

--- wp-content/themes/newtheme/footer.php
+++ wp-content/themes/newtheme/footer.php
@@ -8,16 +8,7 @@
  */
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'newtheme' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'newtheme' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'newtheme' ), 'newtheme', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+</div><!-- #content -->
 
 <?php wp_footer(); ?>
 
--- wp-content/themes/newtheme/template1.php
+++ wp-content/themes/newtheme/template1.php
@@ -10,7 +10,49 @@
 <?php get_header(); ?>
 
 
+<div class="col-lg-8 main-content">
+	<div class="main-content-top row-fluid" >
+		<div class="col-lg-9 fluid">
+			<div class="col-lg-12 top-header fluid ">
+				<ul class="tab-ul list-unstyled">
+					<li>December 2,2014</li>
+					<li>Loui Byrdziak</li>
+				</ul>
+			</div>
+			<div class="col-lg-12 top-content">
+				<ul>
+					<li><a href="#">How to customize Flounder</a></li>
+					<li><a href="#">Common Flounder customizations</a></li>
+					<li><a href="#">Plugin Hooks in Flounder</a></li>
+					<li><a href="#">Changelog</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-lg-3 top-logo-banner">
 
+		</div>
+	</div>
+	<div class="main-content-bot row-fluid" >
+		<div class="col-lg-9 fluid">
+			<div class="col-lg-12 bot-header ">
+				Markup: HTML Tags and Formatting
+			</div>
+			<div class="col-lg-12 bot-content">
 
-<?php get_sidebar(); ?>
+			</div>
+		</div>
+		<div class="col-lg-3 bot-logo-banner fluid">
+			<div class="logo">
+
+			</div>
+			<ul class="list-unstyled">
+				<li>January 11,2013</li>
+				<li>DEMOUSER</li>
+				<li>MARKUP</li>
+				<li>CONTENT, CSS, FORMATTING, HTML, MARKUP</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
 <?php get_footer(); ?>
