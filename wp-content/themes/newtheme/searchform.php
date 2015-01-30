53ab4ca2c36ef1ca4377fdbcb41ad06bc970ccba
wowcyber <wowcyber@yahoo.com>
2015-01-30 10:07:01 +0800

    Updated searchform.php to include html markup

--- wp-content/themes/newtheme/searchform.php
+++ wp-content/themes/newtheme/searchform.php
@@ -1,7 +1,14 @@
 <?php
-/** searchform.php
+/** Custom search form for wordpress.
  * Created by PhpStorm.
  * User: trial
  * Date: 1/30/2015
  * Time: 9:39 AM
  */
+<div class="searchform-container">
+	<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+		<div>
+			<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" class="form-control" placeholder="Search"/>
+		</div>
+	</form>
+</div>
